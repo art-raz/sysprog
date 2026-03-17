82d390460432ce5e43602079326b9eb300b2a321
art-raz <arturrazuvayev39@gmail.com>
2026-03-18 01:05:59 +0300

    test

---
+++ 4/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/home/razuvaev/Desktop/sysprog/utils/unit.cpp.o"
+  "CMakeFiles/test.dir/home/razuvaev/Desktop/sysprog/utils/unit.cpp.o.d"
+  "CMakeFiles/test.dir/test.cpp.o"
+  "CMakeFiles/test.dir/test.cpp.o.d"
+  "CMakeFiles/test.dir/thread_pool.cpp.o"
+  "CMakeFiles/test.dir/thread_pool.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
